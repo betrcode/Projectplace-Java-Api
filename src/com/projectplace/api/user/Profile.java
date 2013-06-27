bc8222eb53c4fdd780c95964bcaf4bab13ff5bfe
Kailashnath Reddy K <kailashnath.k.reddy@gmail.com>
2013-06-27 11:46:18 +0530

    removed the empty constructors

--- src/com/projectplace/api/user/Profile.java
+++ src/com/projectplace/api/user/Profile.java
@@ -8,7 +8,4 @@ public final class Profile extends APIResponse {
 	public String first_name, last_name, city, address2, home_phone, language, title, work_phone, address1, description, email, mobile_phone,
 	avatar, country_code, organisation_homepage, sort_name, homepage, organisation_name, zip_code;
 	public int id;
-	public Profile() {
-		
-	}
 }
--- src/com/projectplace/api/user/Project.java
+++ src/com/projectplace/api/user/Project.java
@@ -5,5 +5,4 @@ import com.projectplace.api.APIResponse;
 public final class Project extends APIResponse {
 	public String membership_type, description, logotype, cost_code, membership_role, name;
 	public int id;
-	public Project () {}
 }
